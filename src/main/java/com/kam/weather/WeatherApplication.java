5188fa0241abd8a6a807395663cd6f49ac7423d6
Kameron-2332 <41551432+Kameron-2332@users.noreply.github.com>
2021-11-29 23:01:42 -0500

    Added retry logic functionality to the HttpstatAPI

--- src/main/java/com/kam/weather/WeatherApplication.java
+++ src/main/java/com/kam/weather/WeatherApplication.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class WeatherApplication {
 
--- src/main/java/com/kam/weather/rest/HttpstatAPI.java
+++ src/main/java/com/kam/weather/rest/HttpstatAPI.java
@@ -5,10 +5,14 @@ import com.kam.weather.util.RestHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -25,6 +29,7 @@ public class HttpstatAPI {
 
     public HttpstatAPI(){}
 
+    @Retryable( value = {HttpStatusCodeException.class, NumberFormatException.class}, maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
     public String getCode(String code){
 
         System.out.println(code);
@@ -32,12 +37,34 @@ public class HttpstatAPI {
         MultiValueMap<String,String> queryParams = new LinkedMultiValueMap<String,String>();
         pathVars.put("codee", code);
         String url = RestHelper.uriBuilder(httpStatAPI, queryParams, pathVars);
-        System.out.println(url);
 
         RestTemplate template = templateConfig.getRestTemplate();
         HttpEntity<Object> entity = RestHelper.buildRequestEntity();
-        ResponseEntity<String> response = template.exchange(url, getCode, entity, String.class);
+        ResponseEntity<String> response;
+
+        try{
+            response = template.exchange(url, getCode, entity, String.class);
+        }catch (HttpStatusCodeException e){
+            response = new ResponseEntity<String>(e.getResponseBodyAsString(), e.getResponseHeaders(), e.getStatusCode());
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+
+            if(response.getStatusCode().is5xxServerError()){
+                System.out.println("Retryyyyyyyyyyyyyyyyyy");
+                throw e;
+            }else if(HttpStatus.TOO_MANY_REQUESTS.equals(response.getStatusCode())){
+                System.out.println("Retryyyyyy TOO MANYYYYY");
+                throw e;
+            }else{
+                System.out.println("FAILLLLLLLLLLLLLLLL");
+                throw e;
+            }
+        }
+
+
+
         return response.getBody();
+
 
     }
 
